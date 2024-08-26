f20a5bc1ac711a603507fe4ea4039603af3ee45e
GabrielR4SH <bieelsantana23@hotmail.com>
2024-08-25 23:30:06 -0300

    feat: AuthGuard in request token

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,20 +3,22 @@ import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { JwtModule } from '@nestjs/jwt';
 import { ConfigModule, ConfigService } from '@nestjs/config';
+import { UsersModule } from 'src/users/users.module';
 
 @Module({
   imports: [
     JwtModule.registerAsync({
       global: true,
       imports: [ConfigModule],
-      inject: [ConfigService],
       useFactory: async (configService: ConfigService) => ({
         secret: configService.get<string>('JWT_SECRET'),
         signOptions: {
           expiresIn: configService.get<string | number>('JWT_EXPIRATION_TIME'),
         },
       }),
+      inject: [ConfigService],
     }),
+    UsersModule,
   ],
   providers: [AuthService],
   controllers: [AuthController],
